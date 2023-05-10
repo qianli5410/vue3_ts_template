a4d16953064d473fb60ada92615c3e8494e7dd26
杨千里 <1696016522@qq.com>
2023-05-10 18:44:22 +0800

    feat：优化

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import * as VueRouter from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import { testRoutes } from './modules/test'
 
 export const routes = [...testRoutes]
-export const router = VueRouter.createRouter({
+export const router = createRouter({
     // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-    history: VueRouter.createWebHistory(),
+    history: createWebHistory(),
     routes
 })
