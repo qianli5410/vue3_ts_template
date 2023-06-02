e05b415998419bf4706708c6d742bae2d39989c1
杨千里 <1696016522@qq.com>
2023-06-02 21:41:03 +0800

    update

---
+++ src/components/Layout/RootNav.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useRouter } from 'vue-router'
+const props = defineProps(['list'])
+const router = useRouter()
+const routerTo = path => {
+	router.push(path)
+}
+</script>
+
+<template>
+	<el-menu class="menu" active-text-color="#FF6600" background-color="#FFFFFF" text-color="#333333" popper-effect="dark">
+		<template v-for="item in props.list" :key="item.path">
+			<el-sub-menu v-if="item.children" :index="item.path">
+				<template #title>
+					<span>{{ item.meta && item.meta.title }}</span>
+				</template>
+				<RootNav :list="item.children"></RootNav>
+			</el-sub-menu>
+			<el-menu-item v-else :index="item.path" @click="routerTo(item.path)">
+				<span>{{ item.meta && item.meta.title }}</span>
+			</el-menu-item>
+		</template>
+	</el-menu>
+</template>
+
+<style scoped lang="scss"></style>
--- src/components/Layout/index.vue
+++ src/components/Layout/index.vue
@@ -1,60 +1,60 @@
 <script lang="ts" setup>
 import { routes } from '@/router/index.ts'
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-
-const routerTo = (path: string) => {
-    router.push(path)
-}
 </script>
 
 <template>
-    <el-container>
-        <el-aside width="200px">
-            <el-menu
-                active-text-color="#ffd04b"
-                background-color="#545c64"
-                class="el-menu-vertical-demo"
-                default-active="2"
-                text-color="#fff"
-            >
-                <template v-for="item in routes" :key="item.path">
-                    <el-sub-menu v-if="item.children" :index="item.path">
-                        <template #title>
-                            <span>{{ item.meta && item.meta.title }}</span>
-                        </template>
-                        <template v-for="item_c in item.children" :key="item_c.path">
-                            <el-menu-item :index="item_c.path" @click="routerTo(item_c.path)">
-                                <span>{{ item_c.meta && item_c.meta.title }}</span>
-                            </el-menu-item>
-                        </template>
-                    </el-sub-menu>
-                    <el-menu-item v-else :index="item.path" @click="routerTo(item.path)">
-                        <span>{{ item.meta && item.meta.title }}</span>
-                    </el-menu-item>
-                </template>
-            </el-menu>
-        </el-aside>
-        <el-main>
-            <router-view v-slot="{ Component }">
-                <transition name="el-zoom-in-center">
-                    <component :is="Component" />
-                </transition>
-            </router-view>
-        </el-main>
-    </el-container>
+	<el-container class="content">
+		<el-header height="70px">
+			<section class="header">Header</section>
+		</el-header>
+		<el-container>
+			<el-aside width="300px">
+				<RootNav :list="routes" />
+			</el-aside>
+			<el-main>
+				<section class="main">
+					<router-view v-slot="{ Component }">
+						<transition name="el-zoom-in-center">
+							<component :is="Component" />
+						</transition>
+					</router-view>
+				</section>
+			</el-main>
+		</el-container>
+	</el-container>
 </template>
 
 <style lang="scss" scoped>
-.el-main,
-.el-menu {
-    height: 100vh;
-    overflow-y: scroll;
-}
+.content {
+	height: 100vh;
+	.el-header,
+	.el-main,
+	.el-aside {
+		padding: 10px;
+	}
+	.el-header {
+		padding-bottom: 0;
+	}
+	.el-aside {
+		padding-right: 0;
+	}
+	.el-menu {
+		height: 100%;
+	}
+	.menu {
+		border-radius: 10px;
+		border-right: none;
+	}
 
-.el-main::-webkit-scrollbar,
-.el-menu::-webkit-scrollbar {
-    display: none;
+	.main {
+		border-radius: 10px;
+		background: #ffffff;
+		padding: 20px;
+	}
+	.header {
+		height: 100%;
+		border-radius: 10px;
+		background: #ffffff;
+	}
 }
 </style>
